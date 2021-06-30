4e967a6d01b6cae348a0912c2407892a23cc4d1d
Mohammed Ziad <Mohammedziad599@gmail.com>
2021-07-01 02:29:00 +0300

    Add Book Model.
    the Book Model has the information about the books which will be stored in the database.

--- BazarCatalogApi/Models/Book.cs
+++ BazarCatalogApi/Models/Book.cs
@@ -1,7 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BazarCatalogApi.Models
 {
     public class Book
     {
-        
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Topic { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public double Price { get; set; }
     }
 }
