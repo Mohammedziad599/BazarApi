a1c1f1ddee84a5d7b77030f1c219dcdabc258c55
Mohammed Ziad <Mohammedziad599@gmail.com>
2021-07-01 03:47:28 +0300

    Created the First Api Endpoint which is the GetBookById.

--- BazarCatalogApi/Controllers/CatalogController.cs
+++ BazarCatalogApi/Controllers/CatalogController.cs
@@ -1,9 +1,24 @@
+using BazarCatalogApi.Data;
+
 using Microsoft.AspNetCore.Mvc;
 
 namespace BazarCatalogApi.Controllers
 {
+    [Route("/")]
     public class CatalogController : Controller
     {
-        
+        private readonly ICatalogRepo _repository;
+
+        public CatalogController(ICatalogRepo repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("book/{id}")]
+        public IActionResult GetBookById(int id)
+        {
+            var bookItem = _repository.GetBookById(id);
+            return Ok(bookItem);
+        }
     }
 }
