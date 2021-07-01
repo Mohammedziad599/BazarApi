4e3663c67e933d5a5b6e441917480f879fee1a6f
Mohammed Ziad <Mohammedziad599@gmail.com>
2021-07-01 06:19:14 +0300

    Created Map Between Book and ReadDto and Update Dto for AutoMapper.

--- BazarCatalogApi/Profiles/CatalogProfile.cs
+++ BazarCatalogApi/Profiles/CatalogProfile.cs
@@ -1,11 +1,17 @@
 using AutoMapper;
 
+using BazarCatalogApi.Dtos;
+using BazarCatalogApi.Models;
+
 namespace BazarCatalogApi.Profiles
 {
     public class CatalogProfile : Profile
     {
         public CatalogProfile()
         {
+            CreateMap<Book, BookReadDto>();
+            CreateMap<Book, BookUpdateDto>();
+            CreateMap<BookReadDto, Book>();
         }
     }
 }
