d9a86cf2379d67862e49fe1462788fb03e258be1
mohammad musleh <mohammadmuslh125@gmail.com>
2021-07-08 01:08:45 +0300

    add dependency injection for IOrderRepo

--- BazarOrderApi/Startup.cs
+++ BazarOrderApi/Startup.cs
@@ -40,6 +40,7 @@ namespace BazarOrderApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo {Title = "BazarOrderApi", Version = "v1"});
             });
+            services.AddScoped<IOrderRepo, SqlOrderRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,7 +52,7 @@ namespace BazarOrderApi
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BazarOrderApi v1"));
             }
-            
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
