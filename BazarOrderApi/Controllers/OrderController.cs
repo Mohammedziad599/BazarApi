900852f7b1d14b2966508ca50b2de54f101194cd
mohammad musleh <mohammadmuslh125@gmail.com>
2021-07-07 23:57:36 +0300

    add order controller

---
+++ BazarOrderApi/Controllers/OrderController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using BazarOrderApi.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BazarOrderApi.Controllers
+{
+    public class OrderController : ControllerBase
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public OrderController(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        [HttpPost("/order/{id}")]
+        public async Task<IActionResult> Orderbook(int id)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://localhost:5001/book/" + id);
+            request.Headers.Add("Accept", "application/json");
+            var client = _clientFactory.CreateClient();
+            var response = await client.SendAsync(request);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var book = await response.Content.ReadFromJsonAsync<Book>();
+
+                if (book?.Quantity > 0)
+                {
+                    var newQuantity = book.Quantity - 1;
+                    var updateRequest =
+                        new HttpRequestMessage(HttpMethod.Patch, "https://localhost:5001/book/update/" + id);
+                    var patchDocument = new JsonPatchDocument();
+                    patchDocument.Replace("/quantity", "" + newQuantity);
+                    updateRequest.Content = new StringContent(patchDocument.ToString());
+                    var updateResponse = await client.SendAsync(updateRequest);
+                    if (updateResponse.StatusCode == HttpStatusCode.NoContent)
+                    {
+                        return Ok();
+                    }
+                }
+                else
+                {
+                    return NotFound("{\"status\":\"Book Out Of Stock\"}");
+                }
+            }
+            else if (response.StatusCode == HttpStatusCode.NotFound)
+            {
+                return NotFound(response.Content);
+            }
+            else if (response.StatusCode == HttpStatusCode.BadRequest)
+            {
+                return BadRequest(response.Content);
+            }
+
+            return BadRequest();
+        }
+    }
+}
