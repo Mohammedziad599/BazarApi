9ad1fef78e0877648feacac89e408bc6b4d96c6e
mohammad musleh <mohammadmuslh125@gmail.com>
2021-07-08 00:41:46 +0300

    add order model

---
+++ BazarOrderApi/Models/Order.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BazarOrderApi.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
