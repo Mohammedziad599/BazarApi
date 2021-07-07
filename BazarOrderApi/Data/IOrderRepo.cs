378e92f3d2982e20f708a93159840038292a02ca
mohammad musleh <mohammadmuslh125@gmail.com>
2021-07-08 00:42:38 +0300

    creat repository interface for order api

---
+++ BazarOrderApi/Data/IOrderRepo.cs
@@ -0,0 +1,10 @@
+﻿using BazarOrderApi.Models;
+
+namespace BazarOrderApi.Data
+{
+    public interface IOrderRepo
+    {
+        public Order AddOrder(Order order);
+        public bool SaveChanges();
+    }
+}
