7e1f65502860ed8ac7632643e1d5b8258728df0e
mohammad musleh <mohammadmuslh125@gmail.com>
2021-07-08 00:48:38 +0300

    add orderdbcontext to Startup

--- BazarOrderApi/Startup.cs
+++ BazarOrderApi/Startup.cs
@@ -2,15 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using BazarOrderApi.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json.Serialization;
 
@@ -28,6 +30,10 @@ namespace BazarOrderApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<OrderDbContext>(options =>
+            {
+                options.UseSqlite(Configuration.GetConnectionString("ConnectionString"));
+            });
             services.AddHttpClient();
             services.AddControllers();
             services.AddSwaggerGen(c =>
