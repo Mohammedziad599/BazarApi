9b486f9d16b6d22d74b633339f43d5b0e4e26037
mohammad musleh <mohammadmuslh125@gmail.com>
2021-07-11 12:42:58 +0300

    add the implementation for SearchByName

--- BazarCatalogApi/Data/MockCatalogRepo.cs
+++ BazarCatalogApi/Data/MockCatalogRepo.cs
@@ -69,6 +69,52 @@ namespace BazarCatalogApi.Data
             return books;
         }
 
+        public IEnumerable<Book> SearchByName(string name)
+        {
+            var books = new List<Book>();
+            books.Add(new()
+            {
+                Id = 0,
+                Name = "Distributed OS",
+                Topic = "Distributed OS",
+                Quantity = 20,
+                Price = 60.0
+            });
+            books.Add(new()
+            {
+                Id = 0,
+                Name = "Distributed OS",
+                Topic = "Distributed OS",
+                Quantity = 20,
+                Price = 60.0
+            });
+            books.Add(new()
+            {
+                Id = 0,
+                Name = "Distributed OS",
+                Topic = "Distributed OS",
+                Quantity = 20,
+                Price = 60.0
+            });
+            books.Add(new()
+            {
+                Id = 0,
+                Name = "Distributed OS",
+                Topic = "Distributed OS",
+                Quantity = 20,
+                Price = 60.0
+            });
+            books.Add(new()
+            {
+                Id = 0,
+                Name = "Distributed OS",
+                Topic = "Distributed OS",
+                Quantity = 20,
+                Price = 60.0
+            });
+            return books;
+        }
+
         public void UpdateBook(Book book)
         {
         }
