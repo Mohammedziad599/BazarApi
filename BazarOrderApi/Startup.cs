1071cc781ca7827f5202b8ff0301ea49f445f4f0
Mohammed Ziad <Mohammedziad599@gmail.com>
2021-07-13 23:42:35 +0300

    Remove Https Redirection in Order Api.

--- BazarOrderApi/Startup.cs
+++ BazarOrderApi/Startup.cs
@@ -70,7 +70,7 @@ namespace BazarOrderApi
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BazarOrderApi v1"));
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
