92bb86d6cc213e17f53c0e7af2aaccac8ec2e879
Mohammed Ziad <Mohammedziad599@gmail.com>
2021-08-08 03:12:36 +0300

    Added a method the insert array to the cache and more.
    also we have added a method to invalidate a key in the cache
    as well as another method to invalidate the search keys
    because if one element changed when searching the search query that
    have that element should be invalid so this method will invalidate
    all the search query.

--- BazarCacheApi/Controllers/CacheController.cs
+++ BazarCacheApi/Controllers/CacheController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using BazarCacheApi.Models;
 
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace BazarCacheApi.Controllers
 {
@@ -13,10 +14,12 @@ namespace BazarCacheApi.Controllers
     public class CacheController : ControllerBase
     {
         private readonly IDictionary<string, Cache> _dictionary;
+        private readonly ILogger<CacheController> _logger;
 
-        public CacheController(IDictionary<string, Cache> dictionary)
+        public CacheController(Dictionary<string, Cache> dictionary, ILogger<CacheController> logger)
         {
             _dictionary = dictionary;
+            _logger = logger;
         }
 
         [HttpGet("{key}")]
@@ -31,20 +34,39 @@ namespace BazarCacheApi.Controllers
                 }
 
                 _dictionary[key].Time = _dictionary[key].Time.AddMinutes(5);
-                return Ok(_dictionary[key].Book);
+                return Ok(_dictionary[key].Book == null ? _dictionary[key].Books : _dictionary[key].Book);
             }
 
             return NotFound();
         }
 
         [HttpPost("{key}")]
-        public IActionResult SetCache(string key, Book book)
+        public IActionResult SetCache(string key, [FromBody] Book book)
         {
             if (ModelState.IsValid)
             {
                 _dictionary[key] = new Cache
                 {
                     Book = book,
+                    Books = null,
+                    Time = DateTime.Now.AddMinutes(30)
+                };
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPost("array/{key}")]
+        public IActionResult SetCache(string key, [FromBody] IEnumerable<Book> books)
+        {
+            _logger.LogInformation("Model State : " + ModelState.IsValid);
+            if (ModelState.IsValid)
+            {
+                _dictionary[key] = new Cache
+                {
+                    Book = null,
+                    Books = books,
                     Time = DateTime.Now.AddMinutes(30)
                 };
                 return Ok();
@@ -57,6 +79,17 @@ namespace BazarCacheApi.Controllers
         public IActionResult InvalidateCache(string key)
         {
             if (_dictionary.ContainsKey(key)) _dictionary.Remove(key);
+
+            return Ok();
+        }
+
+        [HttpPost("invalidateSearches")]
+        public IActionResult InvalidateSearchesCaches()
+        {
+            foreach (var dictionaryKey in _dictionary.Keys)
+                if (dictionaryKey.Contains("s-topic"))
+                    _dictionary.Remove(dictionaryKey);
+                else if (dictionaryKey.Contains("s-name")) _dictionary.Remove(dictionaryKey);
 
             return Ok();
         }
