594d4f02a66125e7f8a76bd13779aa8bee6def7a
Mohammed Ziad <Mohammedziad599@gmail.com>
2021-08-09 15:02:41 +0300

    Added Loggin in the Cache Controller.

--- BazarCacheApi/Controllers/CacheController.cs
+++ BazarCacheApi/Controllers/CacheController.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
 
 using BazarCacheApi.Models;
 
@@ -25,15 +27,18 @@ namespace BazarCacheApi.Controllers
         [HttpGet("{key}")]
         public IActionResult GetCache(string key)
         {
+            _logger.LogInformation($"{DateTime.Now} -- GET /cache/{key} Requested from {Request.Host.Host}");
             if (_dictionary.ContainsKey(key))
             {
-                if (_dictionary[key].Time > DateTime.Now)
+                if (_dictionary[key].Time < DateTime.Now)
                 {
+                    _logger.LogInformation($"{DateTime.Now} -- cache value is out of date for Cache[\"{key}\"]");
                     _dictionary.Remove(key);
                     return NotFound();
                 }
 
-                _dictionary[key].Time = _dictionary[key].Time.AddMinutes(5);
+                _logger.LogInformation($"{DateTime.Now} -- Adding 1 minutes for Cache[\"{key}\"]");
+                _dictionary[key].Time = _dictionary[key].Time.AddMinutes(1);
                 return Ok(_dictionary[key].Book == null ? _dictionary[key].Books : _dictionary[key].Book);
             }
 
@@ -43,8 +48,12 @@ namespace BazarCacheApi.Controllers
         [HttpPost("{key}")]
         public IActionResult SetCache(string key, [FromBody] Book book)
         {
+            _logger.LogInformation($"{DateTime.Now} -- POST /cache/{key} Requested from {Request.Host.Host}");
             if (ModelState.IsValid)
             {
+                _logger.LogInformation(
+                    $"{DateTime.Now} -- setting Cache[\"{key}\"]={JsonSerializer.Serialize(book)}"
+                );
                 _dictionary[key] = new Cache
                 {
                     Book = book,
@@ -54,23 +63,33 @@ namespace BazarCacheApi.Controllers
                 return Ok();
             }
 
+            _logger.LogInformation($"{DateTime.Now} -- Request on /cache/{key} is invalid");
+
             return BadRequest();
         }
 
         [HttpPost("array/{key}")]
         public IActionResult SetCache(string key, [FromBody] IEnumerable<Book> books)
         {
-            _logger.LogInformation("Model State : " + ModelState.IsValid);
+            _logger.LogInformation(
+                $"{DateTime.Now} -- POST /cache/array/{key} Requested from {Request.Host.Host}"
+            );
             if (ModelState.IsValid)
             {
+                var enumerable = books as Book[] ?? books.ToArray();
+
+                _logger.LogInformation(
+                    $"{DateTime.Now} -- setting Cache[\"{key}\"] = {JsonSerializer.Serialize(enumerable)}");
                 _dictionary[key] = new Cache
                 {
                     Book = null,
-                    Books = books,
+                    Books = enumerable,
                     Time = DateTime.Now.AddMinutes(30)
                 };
                 return Ok();
             }
+
+            _logger.LogInformation($"{DateTime.Now} -- Request on /cache/array/{key} is invalid");
 
             return BadRequest();
         }
@@ -78,6 +97,9 @@ namespace BazarCacheApi.Controllers
         [HttpPost("invalidate/{key}")]
         public IActionResult InvalidateCache(string key)
         {
+            _logger.LogInformation(
+                $"{DateTime.Now} -- POST /cache/invalidate/{key} Requested from {Request.Host.Host}"
+            );
             if (_dictionary.ContainsKey(key)) _dictionary.Remove(key);
 
             return Ok();
@@ -86,10 +108,19 @@ namespace BazarCacheApi.Controllers
         [HttpPost("invalidateSearches")]
         public IActionResult InvalidateSearchesCaches()
         {
+            _logger.LogInformation(
+                $"{DateTime.Now} -- POST /cache/invalidateSearches/ Requested from {Request.Host.Host}");
             foreach (var dictionaryKey in _dictionary.Keys)
                 if (dictionaryKey.Contains("s-topic"))
+                {
+                    _logger.LogInformation($"{DateTime.Now} -- Removing Cache[\"{dictionaryKey}\"]");
                     _dictionary.Remove(dictionaryKey);
-                else if (dictionaryKey.Contains("s-name")) _dictionary.Remove(dictionaryKey);
+                }
+                else if (dictionaryKey.Contains("s-name"))
+                {
+                    _logger.LogInformation($"{DateTime.Now} -- Removing Cache[\"{dictionaryKey}\"]");
+                    _dictionary.Remove(dictionaryKey);
+                }
 
             return Ok();
         }
