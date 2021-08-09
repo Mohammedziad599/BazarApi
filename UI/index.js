4809710a08f8d8f08d1b24a8909dd729d13b50c5
Mohammed Ziad <Mohammedziad599@gmail.com>
2021-08-09 14:40:35 +0300

    Added the cache to the deployment and added 3 function to handle the cache operations.
    also we have added documentations for the functions.

--- UI/index.js
+++ UI/index.js
@@ -1,6 +1,4 @@
-const Cache = new Map();
-const ws = new WebSocket("ws://localhost:5000/ws");
-
+let errorModal;
 
 /**
  * this function will used to get the server ip based on user choise in the ui
@@ -16,16 +14,63 @@ function getDeployment(server) {
             if (element[i].value === "Docker") {
                 if (server === "order") {
                     return "localhost:6001";
-                } else {
+                } else if (server === "catalog") {
                     return "localhost:5000";
+                } else if (server === "cache") {
+                    return "localhost:3000";
                 }
             } else {
                 if (server === "order") {
                     return "192.168.50.101";
-                } else {
+                } else if (server === "catalog") {
                     return "192.168.50.100";
+                } else if (server === "cache") {
+                    return "192.168.50.102";
                 }
             }
         }
+    }
+}
+
+/**
+ * this will send a request to the cache to cache the key and the value in the cache server.
+ * @param key the key as a string that represent the value in the cache server
+ * @param value the value that i want to cache in the server
+ * @returns {Promise<void>} nothing
+ */
+async function setCacheValue(key, value) {
+    let cacheServerPath = getDeployment("cache");
+    try {
+        await axios.post(`http://${cacheServerPath}/cache/${key}`, value);
+    } catch (error) {
+    }
+}
+
+/**
+ * this will send a request to the cache to cache the key and the array in the cache server.
+ * @param key the key as a string that represent the array in the cache server
+ * @param arrayOfValues the array that i want to cache in the server
+ * @returns {Promise<void>} nothing
+ */
+async function setCacheArray(key, arrayOfValues) {
+    let cacheServerPath = getDeployment("cache");
+    try {
+        await axios.post(`http://${cacheServerPath}/cache/array/${key}`, arrayOfValues);
+    } catch (error) {
+    }
+}
+
+/**
+ * this function return the value of this key from the cache server
+ * @param key the key as a string that represent the value in the cache server
+ * @returns {Promise<undefined|*>} either undifined if an error happend or it will return an array of books or a book
+ */
+async function getCacheValue(key) {
+    let cacheServerPath = getDeployment("cache");
+    try {
+        let response = await axios.get(`http://${cacheServerPath}/cache/${key}`);
+        return response.data;
+    } catch (error) {
+        return undefined;
     }
 }
