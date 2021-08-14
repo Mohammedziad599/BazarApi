d5e393057103cd31cf939a4b42db3f3d11ff6e0a
Mohammed Ziad <Mohammedziad599@gmail.com>
2021-08-14 23:47:53 +0300

    Write BazarCacheApi Docs.

--- BazarCacheApi/Program.cs
+++ BazarCacheApi/Program.cs
@@ -3,6 +3,9 @@ using Microsoft.Extensions.Hosting;
 
 namespace BazarCacheApi
 {
+    /// <summary>
+    ///     This is the First Class that will be Executed by Dotnet
+    /// </summary>
     public class Program
     {
         public static void Main(string[] args)
@@ -10,6 +13,13 @@ namespace BazarCacheApi
             CreateHostBuilder(args).Build().Run();
         }
 
+        /// <summary>
+        ///     this will create a hostBuilder which can be used to build and run the application
+        ///     the hostBuilder will use the Startup Class in the File Startup.cs
+        ///     which will have our services Configuration and our Http Pipeline Configuration.
+        /// </summary>
+        /// <param name="args">program argument</param>
+        /// <returns>a host builder that will be used to build and run the app</returns>
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
--- BazarCacheApi/Startup.cs
+++ BazarCacheApi/Startup.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 
 using BazarCacheApi.Models;
 
@@ -20,14 +23,36 @@ namespace BazarCacheApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        /// <summary>
+        ///     This Method gets called by the runtime
+        ///     and it is used to add services to the container.
+        /// </summary>
+        /// <param name="services">service collection</param>
         public void ConfigureServices(IServiceCollection services)
         {
+            // This will add the Controllers in the application 
+            // the controller is defined by implementing the ControllerBase or Controller
             services.AddControllers();
+            // Adding Swagger Documentation and Configuring Swagger.
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BazarCacheApi", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Bazar Catalog API",
+                    Description = "A book catalog api",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Mohammed Ziad",
+                        Email = "Mohammedziad599@gmail.com",
+                        Url = new Uri("https://github.com/Mohammedziad599")
+                    }
+                });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
             });
+            // Adding Dictionary as a singleton which means that all the requests will see the same object.
             services.AddSingleton<Dictionary<string, Cache>>();
         }
 
@@ -46,12 +71,17 @@ namespace BazarCacheApi
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BazarCacheApi v1"));
             }
 
+            // we have removed https redirection because it has caused a problem because 
+            // our certificate is self signed this can be turned on in a production environment
             // app.UseHttpsRedirection();
 
+            // Allow Routing in our Controller
             app.UseRouting();
 
+            // Add Authorization in our Http
             app.UseAuthorization();
 
+            // Map The Controllers for their endpoints
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
